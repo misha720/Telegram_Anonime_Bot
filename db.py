1aeef0e48714d2e721b8ec6050c0673e63d6d987
Mishutka <68320483+misha720@users.noreply.github.com>
2022-12-26 13:14:43 +0400

    Update db.py

--- db.py
+++ db.py
@@ -3,11 +3,9 @@ import sqlite3
 conn = sqlite3.connect('base.db')
 cur = conn.cursor()
 
-cur.execute("""CREATE TABLE chat(
-   id INT PRIMARY KEY,
-   user_id INT,
-   message TEXT,
-   file_path TEXT);
-""")
+cur.execute("DELETE FROM users")
+cur.execute("DELETE FROM user_in_search")
+cur.execute("DELETE FROM chat")
+cur.execute("DELETE FROM chats_list")
 
 conn.commit()
